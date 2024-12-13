c3f38fc07d67e729ab6778b2764c0c0880fef8ee
bink0sss <127861415+bink0sss@users.noreply.github.com>
2024-12-13 12:56:58 +0100

    Add files via upload

--- work_with_json.py
+++ work_with_json.py
@@ -7,8 +7,28 @@ def readJSONFilfe():
     print(data["string"])
     print(data["object"]["a"][1])	
 		
+def fromJSONStringToObject():
+  x = '{"k1": 1, "k2": "abc"}'
+  
+  y = json.loads(x)
+  print(y["k2"])
+	
+def fromObjectToJSONString():
+  x = {
+    "k1": 1,
+    "k2": "abc"
+		}
+  y = json.dumps(x)
+  print(y)
+  print(type(y))
+def fromJSONToCSV():
+  with open("12.json","rt") as fData:
+    data = json.load(fData)
+    print(data[object]["timeServer"])
 def main():
-  readJSONFilfe()
-
+  #readJSONFilfe()
+  #fromJSONStringToObject()
+  #fromObjectToJSONString()
+  fromJSONToCSV()
 if __name__ == "__main__":
   main()
